8aacf84a723df331c06897ff1992e0e42bfaa961
simplyana08 <ana.t08@yahoo.com>
2026-03-20 10:34:12 +0100

    git conflicts

--- GameForm.Designer.cs
+++ GameForm.Designer.cs
@@ -29,13 +29,23 @@
         private void InitializeComponent()
         {
             splitContainer1 = new SplitContainer();
+<<<<<<< Updated upstream
+=======
+            textBox2 = new TextBox();
+            button2 = new Button();
+            button1 = new Button();
+            textBox1 = new TextBox();
+>>>>>>> Stashed changes
             button6 = new Button();
             button5 = new Button();
             button4 = new Button();
             button3 = new Button();
+<<<<<<< Updated upstream
             button2 = new Button();
             button1 = new Button();
             textBox1 = new TextBox();
+=======
+>>>>>>> Stashed changes
             ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
             splitContainer1.Panel1.SuspendLayout();
             splitContainer1.Panel2.SuspendLayout();
@@ -65,6 +75,49 @@
             splitContainer1.TabIndex = 0;
             splitContainer1.SplitterMoved += splitContainer1_SplitterMoved;
             // 
+<<<<<<< Updated upstream
+=======
+            // textBox2
+            // 
+            textBox2.Location = new Point(-16, 12);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(507, 39);
+            textBox2.TabIndex = 6;
+            textBox2.Text = "BlackJack";
+            textBox2.TextAlign = HorizontalAlignment.Center;
+            // 
+            // button2
+            // 
+            button2.ForeColor = Color.DarkOliveGreen;
+            button2.Location = new Point(31, 625);
+            button2.Name = "button2";
+            button2.Size = new Size(150, 46);
+            button2.TabIndex = 1;
+            button2.Text = "Hit";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.ForeColor = Color.IndianRed;
+            button1.Location = new Point(269, 625);
+            button1.Name = "button1";
+            button1.RightToLeft = RightToLeft.Yes;
+            button1.Size = new Size(150, 46);
+            button1.TabIndex = 0;
+            button1.Text = "Stand";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(61, 12);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(554, 39);
+            textBox1.TabIndex = 2;
+            textBox1.Text = "BlackJack";
+            textBox1.TextAlign = HorizontalAlignment.Center;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+>>>>>>> Stashed changes
             // button6
             // 
             button6.Location = new Point(497, 466);
@@ -102,6 +155,7 @@
             button3.Text = "New Round";
             button3.UseVisualStyleBackColor = true;
             // 
+<<<<<<< Updated upstream
             // button2
             // 
             button2.ForeColor = Color.DarkOliveGreen;
@@ -133,13 +187,19 @@
             textBox1.TextAlign = HorizontalAlignment.Center;
             textBox1.TextChanged += textBox1_TextChanged;
             // 
+=======
+>>>>>>> Stashed changes
             // GameForm
             // 
             AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
+<<<<<<< Updated upstream
             BackColor = Color.DarkSeaGreen;
             ClientSize = new Size(1166, 754);
             Controls.Add(splitContainer1);
+=======
+            ClientSize = new Size(1166, 787);
+>>>>>>> Stashed changes
             Name = "GameForm";
             Text = "GameForm";
             splitContainer1.Panel1.ResumeLayout(false);
@@ -148,6 +208,21 @@
             ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
             splitContainer1.ResumeLayout(false);
             ResumeLayout(false);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void splitContainer1_SplitterMoved(object sender, SplitterEventArgs e)
+        {
+            throw new NotImplementedException();
         }
 
         #endregion
