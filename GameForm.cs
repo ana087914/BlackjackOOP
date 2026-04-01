2afc7136bf330670af3d8afaa14587d096415926
simplyana08 <ana.t08@yahoo.com>
2026-04-01 13:06:39 +0200

    GameFormLoad toegepaast, functies StartNewRound en UpdateUi gemaakt en ik heb nog 3 pictureBox gemaakt

--- GameForm.cs
+++ GameForm.cs
@@ -16,6 +16,9 @@ using System.Windows.Forms;
         {
             InitializeComponent();
         }
+        private Shoe shoe;
+        private Hand playerHand;
+        private Hand dealerHand;
         private string GetCardImagePath(Card card)
         {
             string rank = "";
@@ -43,10 +46,55 @@ using System.Windows.Forms;
         }
         private void GameForm_Load(object sender, EventArgs e)
         {
-            Card testCard = new Card(Rank.ACE, Suit.SPADES);
+            shoe = new Shoe(2);
+            playerHand = new Hand();
+            dealerHand = new Hand();
 
-            pictureBox1.Image = Image.FromFile(
-@"C:\Users\anapr\Documents\schooljaar2\periode3\OOP\BlackjackOOP\Cards\ace_of_spades.png");
+            StartNewRound();
+        }
+        private void StartNewRound()
+        {
+            if (shoe.Count < 10)
+            {
+                shoe = new Shoe(2);
+            }
+
+            playerHand.Clear();
+            dealerHand.Clear();
+
+            playerHand.AddCard(shoe.DealCard());
+            dealerHand.AddCard(shoe.DealCard());
+            playerHand.AddCard(shoe.DealCard());
+            dealerHand.AddCard(shoe.DealCard());
+
+            UpdateUI();
+
+            if (playerHand.GetValue() == 21)
+            {
+                MessageBox.Show("Blackjack! Player wins.");
+            }
+        }
+        private void UpdateUI()
+        {
+            pictureBox1.Image = null;
+            pictureBox2.Image = null;
+            pictureBox3.Image = null;
+            pictureBox4.Image = null;
+
+            if (playerHand.Cards.Count > 0)
+                pictureBox1.Image = Image.FromFile(GetCardImagePath(playerHand.Cards[0]));
+
+            if (playerHand.Cards.Count > 1)
+                pictureBox2.Image = Image.FromFile(GetCardImagePath(playerHand.Cards[1]));
+
+            if (dealerHand.Cards.Count > 0)
+                pictureBox3.Image = Image.FromFile(GetCardImagePath(dealerHand.Cards[0]));
+
+            if (dealerHand.Cards.Count > 1)
+                pictureBox4.Image = Image.FromFile(GetCardImagePath(dealerHand.Cards[1]));
+
+            textBox2.Text = "Player: " + playerHand.GetValue().ToString();
+            textBox1.Text = "Dealer: " + dealerHand.GetValue().ToString();
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
